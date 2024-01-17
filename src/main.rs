5fc72efcb7d58549063463852c61a53cce915071
Kavya Anbarasu <kavya.anbarasu@gmail.com>
2024-01-17 15:32:55 -0500

    learned about variables, scalars, compounds datatypes, fucns, comments, loops, in rust! implemented fib

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,91 @@
+// fn main() {
+//     let mut x = 5;
+//     println!("The value of x is: {x}");
+//     x = 6;
+//     println!("The value of x is: {x}");
+// }
+
+// use std::io;
+
+// fn main() {
+//     let a = [1, 2, 3, 4, 5];
+
+//     println!("Please enter an array index.");
+
+//     let mut index = String::new();
+
+//     io::stdin()
+//         .read_line(&mut index)
+//         .expect("Failed to read line");
+
+//     let index: usize = index
+//         .trim()
+//         .parse()
+//         .expect("Index entered was not a number");
+
+//     if index > 4 {
+//         println!("Invalid array index entered");
+//     }
+//     else {
+//         let element = a[index];
+//         println!("The value of the element at index {index} is: {element}");
+//     }
+// }
+
+// fn main() {
+//     println!("Hello, world!");
+
+//     another_function(5);
+// }
+
+// fn another_function(x: i32) {
+//     println!("The value of x is: {x}");
+// }
+
+// fn five() -> i32 {
+//     5
+// }
+
+// fn main() {
+//     let x = five();
+
+//     println!("The value of x is: {x}");
+// }
+
+// fn main() {
+//     let mut count = 0;
+//     'counting_up: loop {
+//         println!("count = {count}");
+//         let mut remaining = 10;
+
+//         loop {
+//             println!("remaining = {remaining}");
+//             if remaining == 9 {
+//                 break;
+//             }
+//             if count == 2 {
+//                 break 'counting_up;
+//             }
+//             remaining -= 1;
+//         }
+
+//         count += 1;
+//     }
+//     println!("End count = {count}");
+// }
+
+// generate the nth Fibonacci number
 fn main() {
-    let mut x = 5;
-    println!("The value of x is: {x}");
-    x = 6;
-    println!("The value of x is: {x}");
+    println!("The 10th Fibonacci number is: {}", fib(10));
+}
+
+fn fib(n: i32) -> i32 {
+    if n == 0 {
+        return 0;
+    }
+    if n == 1 {
+        return 1;
+    }
+
+    fib(n - 1) + fib(n - 2)
 }
